f2b23e469cb6f1374fb06747363ffc7cb234fe74
Reenatechie <kareenajatiton@gmail.com>
2026-06-16 08:55:46 +0100

    Configure remote Terraform S3 backend

---
+++ terraform/backend/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project = "karatu-2025-capstone"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
